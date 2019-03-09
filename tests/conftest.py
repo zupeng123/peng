d720682615835e3a229d61a1e8798622850b7bf5
Pablo Acosta-Serafini <pmasdev@gmail.com>
2019-03-09 09:08:57 -0500

    Package management framework update

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 # conftest.py
 # Copyright (c) 2013-2019 Pablo Acosta-Serafini
 # See LICENSE for details
-# pylint: disable=C0111,C0411,C0413,E0012,E1101,E1103,F0401,W0212
+# pylint: disable=C0111,C0411,C0413,E0012,E0611,E1101,E1103,F0401,W0212
 
 # Standard library imports
 import os
